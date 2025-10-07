72082e0acf41097c2827f01e112c003498f453cd
gamecraftai000 <gamecraftai000@gmail.com>
2025-10-07 21:38:36 +0300

    Update generate-image.js

--- pages/api/generate-image.js
+++ pages/api/generate-image.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     {
       method: 'POST',
       headers: {
-        'Authorization': 'Bearer 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',
+        'Authorization': 'Bearer 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',
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
